48ad518283f427c8a6ecd498f8a086cd57b091d6
Evakulenko <evgenijyuri4@gmail.com>
2015-08-29 11:57:08 +0300

    q2

---
+++ src/Task2/Employee.java
@@ -0,0 +1,18 @@
+package Task2;
+
+/**
+ * Created by Евгений on 29.08.2015.
+ */
+public class Employee {
+
+    String fullName,passNumber;
+    long iNN;
+
+    public Employee(String fullName, String passNumber, long INN){
+        this.fullName = fullName;
+        this.passNumber = passNumber;
+        this.iNN = iNN;
+    }
+
+
+}
