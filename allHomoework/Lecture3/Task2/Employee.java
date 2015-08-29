979296ab572a43277294216ac84d36f1aa878e28
Evakulenko <Ineedadollar13>
2015-08-29 15:59:54 +0300

    dfdd

--- allHomoework/Lecture3/Task2/Employee.java
+++ allHomoework/Lecture3/Task2/Employee.java
@@ -5,10 +5,14 @@ package Lecture3.Task2;
  */
 public class Employee {
 
+//    Класс сотрудники это отдельный класс может быть...либо можно хард кодом накидать сразу в лист
+//    По поводу задания с мапами и сотрудниками...вывести кол-во сотрудников в отделе;
+//    Выбрать по инн(или чему-нибудь иному) И тд
+
     String fullName,passNumber;
     long iNN;
 
-    public Employee(String fullName, String passNumber, long INN){
+    public Employee(String fullName, String passNumber, long iNN){
         this.fullName = fullName;
         this.passNumber = passNumber;
         this.iNN = iNN;
--- allHomoework/Lecture3/Task2/Main.java
+++ allHomoework/Lecture3/Task2/Main.java
@@ -9,23 +9,33 @@ public class Main {
 //    1. Есть сотрудники, есть отделы, в которых сотрудники эти работают  можно поюзать мапу + линкедлист+ дженерики
 //    ключем мапы будет отдел, валью - лист сотрудников   2. по очередям.. напрмер Заказы...или Документы
 //
-//    поюзать все методы
 //    т.е. в класс Сотрудники мы можем добавить поля ФИО, паспорт.данные, инн ....
 //    и потом поиграться с ними... доставать по какому-либо параметру добавлять, сортировать и т.д.
 //
-//    Класс сотрудники это отдельный класс может быть...либо можно хард кодом накидать сразу в лист
-//    По поводу задания с мапами и сотрудниками...вывести кол-во сотрудников в отделе;
-//    Выбрать по инн(или чему-нибудь иному) И тд
-//
-//    Москаленко Иван: я думаю что Елена предпологала что бы мы сделали так: Ключ: Отдел (Строка)
-//    Значение : ArrayList<Класс Сотрудники>. Т.е. значением будет массив Сотрудников.
-//    Например Ключ: Бухгалтерия  Значение [Иванова Н.Л., Петрова М.А.] Если я не прав поправте!
+//    Например Ключ: Бухгалтерия  Значение [Иванова Н.Л., Петрова М.А.]
 //    Значение-Лист,в кот укладываются сотпулники Map<String, List<Employee>>как-то так примерно
 
     public static void main(String[] args) {
         HashMap<String, ArrayList<Employee>> hashMap = new HashMap<>();
-        hashMap.size();
 
-    }
+        ArrayList<Employee> list1 = new ArrayList<Employee>();
+        list1.add(new Employee("Ivanov V V ","AE894456", 45562366));
+        list1.add(new Employee("Petrov P.P.", "AE872836", 54421665));
+        list1.add(new Employee("Vasyl`ev Nikolay Nikolayevich ", "AE894456", 54545435));
+        hashMap.put("salesDepartment", list1);
+
+        ArrayList<Employee> list2 = new ArrayList<Employee>();
+        list2.add(new Employee("Lutsenko Yurii Yurievich","AE894343", 80987989));
+        list2.add(new Employee("Yaresko Natalia Andreevna","AE894565", 879879879));
+        list2.add(new Employee("Yatsenyk Arsinii Petrovich","AE894268", 89789898));
+        hashMap.put("salesDepartment", list2);
+
+        ArrayList<Employee> list3 = new ArrayList<Employee>();
+        list3.add(new Employee("Gontareva Olga Olegovna","AE894353", 67676576));
+        Employee emloyee8 = new Employee("Poroshenko Petr Petrovish ","AE894375", 454646464);
+        Employee emloyee9 = new Employee("Zhukov Zkuk Zhuk ","AE894452", 399393939);
+        hashMap.put("financialDepartment", list3);
+
+        }
 
 }
